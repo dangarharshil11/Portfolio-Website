a2bfb7724615a555aafdaaf0d28abf0091aab3c1
Harshil Dangar <dangarharshil11@gmail.com>
2023-12-06 18:18:29 +0530

    Footer added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
         <CustomLink href="/about" title="About" className="mx-4" />
         <CustomLink href="/projects" title="Projects" className="mx-4" />
         <CustomLink href="/articles" title="Articles" className="mx-4" />
-        <CustomLink href="/contact" title="Articles" className="ml-4" />
+        <CustomLink href="/contact" title="Contact" className="ml-4" />
       </nav>
 
       <nav className="flex items-center justify-center flex-wrap">
